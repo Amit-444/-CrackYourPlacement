140912dbb773ddb0c4afd35906643f0b1aeed83c
Amit-444 <amitbeherazx@gmail.com>
2024-08-02 23:44:03 +0530

    Day 11 Completed

--- Day 11/CeilingInASortedArray.java
+++ Day 11/CeilingInASortedArray.java
@@ -1,6 +1,8 @@
 // Ceiling in a sorted array
 
 public class CeilingInASortedArray {
+
+    // BinarySearch Approach
     public static int ceilingVal(int arr[], int x) {
         int start = 0;
         int end = arr.length - 1;
@@ -24,9 +26,26 @@ public class CeilingInASortedArray {
 
         return ans;
     }
+
+    // Bruteforce Approach
+    // public static int ceilingVal(int arr[], int x) {
+    //     int ans = -1;
+
+    //     for(int i=0; i<arr.length; i++) {
+    //         if(arr[i] == x)
+    //             ans = arr[i];
+    //         else if(arr[i] > x) {
+    //             ans = arr[i];
+    //             break;
+    //         }
+    //     }   
+
+    //     return ans;
+    // }
+
     public static void main(String[] args) {
         int arr[] = {1, 2, 8, 10, 10, 12, 19};
-        int x = 19;
+        int x = 5;
 
         System.out.println("The Ceilling value of " + x + " is : " + ceilingVal(arr, x));
     }
